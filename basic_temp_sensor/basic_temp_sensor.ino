a30f95da272de9d484270b8654c52d41039264ea
thomas mckelvey <thomas mckelvey>
2017-01-15 20:07:51 +0000

    Added code for LED to turn on if greater than or equal to 25C
    Signed-off-by: thomas mckelvey <thomas mckelvey>

--- basic_temp_sensor/basic_temp_sensor.ino
+++ basic_temp_sensor/basic_temp_sensor.ino
@@ -2,6 +2,7 @@
 #include <DallasTemperature.h>
 /*-----( Declare Constants )-----*/
 #define ONE_WIRE_BUS 2 /*-(Connect to Pin 2 )-*/
+#define RED_LED 3
 
 int temp = 0;
 /*-----( Declare objects )-----*/
@@ -14,6 +15,8 @@ void setup() /*----( SETUP: RUNS ONCE )----*/
 {
 delay(1000);
 Serial.begin(9600);
+pinMode(RED_LED, OUTPUT);
+digitalWrite(RED_LED, LOW);
 Serial.println("YourDuino.com: Electronic Brick Test Program");
 Serial.println("Temperature Sensor DS18B20");
 delay(1000);
@@ -25,7 +28,13 @@ void loop() /*----( LOOP: RUNS CONSTANTLY )----*/
 Serial.println();
 sensors.requestTemperatures(); // Send the command to get temperatures
 Serial.print("Device 1 (index 0) = ");
-Serial.print(sensors.getTempCByIndex(0));
+temp = sensors.getTempCByIndex(0);
+Serial.print(temp);
 Serial.println(" Degrees C");
+if(temp >= 25){
+    digitalWrite(RED_LED, HIGH);
+}else{
+    digitalWrite(RED_LED, LOW);
+}
 delay(1000);
 }
